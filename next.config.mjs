0d78e82fa1e78465d1b21b4e05e55a5e4c3f8875
pmfcastilho <pmfcastilho@icloud.com>
2025-06-27 13:04:20 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,3 +4,17 @@ const nextConfig = {
 };
 
 export default nextConfig;
+export default function Home() {
+  return (
+    <main className="w-screen h-screen">
+      <iframe
+        src="https://my.spline.design/claritystream-prmHX2kmE8ZqWdyyCYdOt3bu/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        allow="fullscreen"
+        style={{ border: 'none' }}
+      />
+    </main>
+  );
+}
